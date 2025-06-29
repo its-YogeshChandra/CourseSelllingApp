af1a09d5413be75ae4f72a9aa776f015c29eba33
LG-Crusty <pandittheroyal@gmail.com>
2025-06-29 19:09:44 +0530

    current one

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -15,9 +13,17 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8080/",
         changeOrigin: true,
       },
     },
   },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["lightningcss", "fsevents"],
+  //   },
+  // },
+  // optimizeDeps: {
+  //   exclude: ["lightningcss", "fsevents"],
+  // },
 });
