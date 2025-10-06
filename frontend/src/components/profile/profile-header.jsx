d161481ef17c526ccaf0c82303587138edda1411
LG-Crusty <pandittheroyal@gmail.com>
2025-10-06 17:11:49 +0530

    saving commit

--- frontend/src/components/profile/profile-header.jsx
+++ frontend/src/components/profile/profile-header.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export function ProfileHeader() {
   const [profile, setProfile] = useState({
     name: "random",
+    email: "random@gmail.com",
     avatarUrl:
       "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     subscription: {
