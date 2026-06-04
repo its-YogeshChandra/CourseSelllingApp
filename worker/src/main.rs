f4bf536a3dda1cdd83341b976b015f6573b73bf1
its-YogeshChandra <pandittheroyal@gmail.com>
2026-06-05 02:29:56 +0530

    feat: integrate lapin for RabbitMQ messaging and add dotenvy for configuration management

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -3,8 +3,22 @@ mod consumer;
 mod handlers;
 mod models;
 use tokio;
+use lapin :: {
+   BasicProperties, Connection, ConnectionProperties, Result, options::*, types::FieldTable
+};
+use dotenvy::dotenv;
+use std::env;
+//conect to the rabbit mq using lapin 
 
 #[tokio::main]
 async fn main() {
-   //talk to the message queue from rabbit mq  
+  dotenv().ok();
+    let conn_addr = std::env::var("RABBITMQ_URL").expect("RABBITMQ_URL must be set");
+    let runtime = lapin::runtime::default_runtime().unwrap();
+
+    let connection = Connection::connect(&conn_addr , ConnectionProperties::default()).await.unwrap();
+    let channel = connection.create_channel().await.unwrap();
+
+    let mut consumer = channel.basic_consume("videoProcessing".into(), "my-worker".into(),BasicConsumeOptions::default(), FieldTable::default());
+     
     }
